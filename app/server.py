6134ee033df5675de98b5874f67a2e26018e36c5
Lucas Menendez <epucas@gmail.com>
2017-02-27 10:35:39 +0100

    debuggin bot

--- app/server.py
+++ app/server.py
@@ -204,7 +204,7 @@ def bot_send_message(user_id, content):
 		}
 	}
 	resp = requests.post("https://graph.facebook.com/v2.8/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-	print resp
+	print(resp)
 	
 
 
