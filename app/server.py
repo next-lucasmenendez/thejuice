cf30da095086bb681d97f91c7710bfd5dc9304c4
Lucas Menendez <epucas@gmail.com>
2017-02-27 10:45:17 +0100

    debuggin bot

--- app/server.py
+++ app/server.py
@@ -204,7 +204,7 @@ def bot_send_message(user_id, content):
 		}
 	}
 	resp = requests.post("https://graph.facebook.com/v2.8/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-	print(resp.__dict__)
+	print(resp["status_code"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
