6802fd40ab0e4e4d2f089bafd2a844e838b4b94a
Axel Blanco <axel.blanco@beeva.com>
2017-06-05 17:54:39 +0200

    JS controllers and templates

---
+++ static/js/app/controllers/login.js
@@ -0,0 +1,45 @@
+/**
+ * Created by lucas.menendez on 17/5/17.
+ */
+
+var app = angular.module('app');
+
+app.controller('loginCtrl', function ($window, $rootScope, $scope, $state, service) {
+	$scope.submitLoginForm = function() {
+		var config	= {scope: "public_profile, email", return_scopes: !0},
+			attrs	= {fields: 'name, email,picture'};
+
+		FB.login(function (response) {
+			if (response.authResponse !== null) {
+				FB.api('/me', attrs, function(profile) {
+					var data = {
+						id: profile.id,
+						email: profile.email,
+						name: profile.name,
+						accesstoken: response.authResponse.accessToken,
+						picture: profile.picture.data.url
+					}
+
+					service.request('POST', '/login', data, true).then(
+						function () {
+							$window.ga('send', 'event', 'auth', 'login', data.email);
+
+							sessionStorage.setItem('id', data.id);
+							sessionStorage.setItem('name', data.name);
+							sessionStorage.setItem('email', data.email);
+							sessionStorage.setItem('accesstoken', data.accesstoken);
+							sessionStorage.setItem('picture', data.picture);
+
+							$state.go('base.search');
+						},
+						function (error) {
+							$rootScope.$broadcast('showNotification', error.data.message);
+						}
+					);
+				});
+			} else {
+				$rootScope.$broadcast('showNotification', "Something was wrong on login. Please try again.");
+			}
+		}, config);
+	}
+});
---
+++ static/js/app/controllers/search.js
@@ -0,0 +1,46 @@
+/**
+ * Created by lucas.menendez on 16/5/17.
+ */
+
+var app = angular.module('app');
+
+
+app.controller('searchCtrl', function ($window, $rootScope, $scope, $state, service, route, DataStorage) {
+	$scope.$on('$stateChangeSuccess', function () {
+		var current = route.Link('Search', 'search', 'base.search');
+
+		route.clear();
+		route.setCurrent(current);
+		route.apply();
+		DataStorage.clear()
+	});
+
+	$scope.lang = "en";
+	$scope.query = "";
+	$scope.submitSearchForm = function() {
+        $rootScope.$broadcast('showSpinner', 'Wait a second while we take the data out of Wikipedia...');
+        $window.ga('send', 'event', 'index', 'search', $scope.query);
+        service.request('POST', '/search', {query: $scope.query, lang: $scope.lang}).then(
+            function (response) {
+                if (response.success) {
+                    DataStorage.set($scope.query, response.results);
+                    DataStorage.set("lang", $scope.lang);
+                    $state.go('base.review', {query: $scope.query});
+                } else {
+                    $scope.results = response.options;
+                }
+                $rootScope.$broadcast('hideSpinner');
+            },
+            function (error) {
+                $rootScope.$broadcast('hideSpinner');
+                $rootScope.$broadcast('showNotification', error.data.message);
+                $state.go('base.search');
+            }
+        );
+	}
+
+	$scope.forceSearchForm = function(query) {
+		$scope.query = query;
+		$scope.submitSearchForm(true);
+	}
+});
