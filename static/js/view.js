45705bca0ebfad32cf92f6b2b651de83cf8a61a4
Axel Blanco <axel.blanco@beeva.com>
2017-06-08 17:26:21 +0200

    Logic for Trivia resolution

--- static/js/view.js
+++ static/js/view.js
@@ -4,6 +4,29 @@
 
 $(function() {
     randomizeAnswers();
+
+    $('.submit button').on('click', function(e) {
+    	var email = checkEmail();
+    	var questions = checkQuestions();
+
+    	if (email && questions) {
+    		$.ajax({
+				url: '/validate',
+				type: 'POST',
+				data: JSON.stringify({
+	                studentEmail: email,
+					teacherEmail: $('.submit input#teacherEmail').val(),
+					title: $('.header .text .name').text() + ' ' + $('.header .text .surname').text(),
+				    questions: questions
+		        }),
+			    contentType: 'application/json; charset=utf-8',
+			    dataType: 'json',
+				success: function(success) {
+					showCongratulations(success.score);
+				}
+			});
+	    }
+    });
 });
 
 function randomizeAnswers() {
@@ -12,4 +35,74 @@ function randomizeAnswers() {
             return Math.round(Math.random()) - 0.5;
         }).detach().appendTo(this);
 	});
+}
+
+function checkEmail() {
+	var email = $('.submit input#studentEmail').val();
+
+	if (email) {
+		if (email.match(/.+@.+\..+/g)) {
+			removeError('email');
+			return email;
+		}
+	}
+
+	printErrors('email', 'Insert a valid email address.');
+	return false;
+}
+
+function checkQuestions() {
+	var questions = [];
+	var error = false;
+	$('.question').each(function() {
+		if ($(this).find('input:checked').length === 0) {
+			error = true;
+			return false;
+		}
+		questions.push({
+			question: $(this).find('.text').text(),
+			correct: $(this).find('.answers').find('label[data="True"]').text(),
+			select: $(this).find('input:checked').attr('id')
+		});
+	});
+
+	if (error) {
+		printErrors('questions', 'Some answers need to be completed.');
+		return false;
+	} else {
+		removeError('questions');
+	}
+
+	return questions;
+}
+
+function showCongratulations(score) {
+	$('.submit').remove();
+	$('.question').remove();
+	$('.header').remove();
+	$('.questions').prepend(
+		'<div class="congrats">' +
+		'<h1>Congratulations</h1>' +
+		'<p>You have finished the Trivia.</p>' +
+		'<p>Your score was <span class="score">' + score + '%</span></p>' +
+		'</div>'
+	);
+
+}
+
+function printErrors(id, error) {
+	if ($('.error #' + id).length === 0) {
+		$('.error').append('<li id="' + id + '">' + error + '</li>');
+		$('.error').slideDown();
+	}
+
+}
+
+function removeError(id) {
+	if ($('.error #' + id).length > 0) {
+		$('.error #' + id).remove();
+	}
+	if ($('.error')[0].childNodes.length === 0) {
+		$('.error').slideUp();
+	}
 }
