5ef2a6895c0a3170f5f5989e3be7744cc633bf1c
Axel Blanco <axel.blanco@beeva.com>
2017-06-19 16:59:44 +0200

    review process

--- static/js/app/controllers/review.js
+++ static/js/app/controllers/review.js
@@ -4,11 +4,11 @@
 
 var app = angular.module("app");
 
-app.controller('reviewCtrl', function ($window, $rootScope, $scope, $state, $stateParams, $timeout, service, tracker, route, DataStorage) {
+app.controller('reviewCtrl', function ($window, $rootScope, $scope, $state, $stateParams, $timeout, requests, tracker, route, DataStorage) {
 	$scope.$on('$stateChangeSuccess', function () {
 		var prev = route.Link('Search', 'search', 'base.search'),
 			current = route.Link('Review', 'review', 'base.review'),
-			next = route.Button('Create', 'download', $scope.download);
+			next = route.Button('Design', 'design', $scope.design);
 
 		route.clear();
 		route.setPrev(prev);
@@ -18,30 +18,12 @@ app.controller('reviewCtrl', function ($window, $rootScope, $scope, $state, $sta
 	});
 
 	$scope.showEdit	= false;
-	$scope.query = $stateParams.query;
-	if ($scope.query) {
-		$scope.result = DataStorage.get($scope.query);
-		$scope.lang = DataStorage.get("lang") || 'en';
-	} else {
-        $rootScope.$broadcast('showNotification', 'An error ocurred. Please, try again.');
-		$state.go('base.search');
-	}
+	$scope.result = DataStorage.get('result');
 
-	$scope.edit = function (question) {
-		var empty = !Boolean(question);
-		if (empty) {
-			$scope.current = {};
-			$scope.current.answers = [
-				{name: '', correct: false},
-				{name: '', correct: false},
-				{name: '', correct: false},
-				{name: '', correct: true}
-			]
-		} else {
-			$scope.current = question;
-			$scope.current.first_question = question.question.split(/\s*_+\s*/)[0];
-			$scope.current.second_question = question.question.split(/\s*_+\s*/)[1];
-		}
+	$scope.edit = function (sentence) {
+		$scope.current = {};
+		$scope.current.old = sentence;
+		$scope.current.new = sentence;
 		$scope.showEdit	= true;
 	};
 
@@ -50,46 +32,27 @@ app.controller('reviewCtrl', function ($window, $rootScope, $scope, $state, $sta
 		$scope.showEdit	= false;
 	};
 
-	$scope.delete = function (question) {
-		var index = $scope.result.questions.indexOf(question);
-		$scope.result.questions.splice(index, 1);
+	$scope.delete = function (sentence) {
+		var index = $scope.result.sentences.indexOf(sentence);
+		$scope.result.sentences.splice(index, 1);
 	};
 
 	$scope.save = function () {
-		$scope.current.question = $scope.current.first_question.concat(' __________ ', $scope.current.second_question);
+		$window.ga('send', 'event', 'review', 'event edited');
+		tracker.all('review', 'event', 'edited');
 
-		if (!$scope.current.hasOwnProperty('$$hashKey')) {
-			$scope.result.questions.push(angular.copy($scope.current));
-			tracker.all('review', 'question', 'created');
-		} else {
-			tracker.all('review', 'question', 'edited');
-		}
+		var index = $scope.result.sentences.indexOf($scope.current.old);
+		$scope.result.sentences[index] = $scope.current.new;
+
 		$scope.showEdit	= false;
 	};
 
-	$scope.download = function() {
-		if ($scope.result.questions) {
-			tracker.all('review', 'submited', $scope.query);
-			var email = sessionStorage.getItem('email');
-			var result = angular.copy($scope.result);
-
-			service.request("POST", "/download", {
-				result: result,
-				email: email
-			}, true).then(
-				function (response) {
-					if (response.success) {
-						$window.location.href = response.result;
-					} else {
-						$state.go('base.search');
-					}
-					$rootScope.$broadcast('hideSpinner');
-				},
-				function (error) {
-					$rootScope.$broadcast('hideSpinner');
-					$rootScope.$broadcast('showNotification', error.data.message);
-				}
-			);
+	$scope.design = function() {
+		var query = $stateParams.query || $scope.query;
+		tracker.all('review', 'submited', $scope.result.title);
+		if ($scope.result) {
+			DataStorage.set('article', $scope.result);
+			$state.go('base.design');
 		}
-	}
+	};
 });
